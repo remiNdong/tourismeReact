b11911dcdc11351bd6d2d61091b26cd9a1ef39a4
remiNdong <ndongremi@aol.fr>
2022-08-08 19:18:16 +0200

    first commit

---
+++ TOURISME_REACT/src/main/java/fr/tourisme/react/TourismeReactApplication.java
@@ -0,0 +1,13 @@
+package fr.tourisme.react;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourismeReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TourismeReactApplication.class, args);
+	}
+
+}
---
+++ TOURISME_REACT/src/test/java/fr/tourisme/react/TourismeReactApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.tourisme.react;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourismeReactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
